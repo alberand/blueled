60bf70eb5c5cbe5800b527342fc8f7a5406c53d8
alberand <albershteyn.andrey@gmail.com>
2020-05-02 15:05:33 +0200

    fix: data conversion in macro

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,7 +4,7 @@
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
 #define get_u8(d) ((uint8_t)(d[0]))
-#define get_u16(d) ((uint16_t)((d[0] << 8) | d[1]))
+#define get_u16(d) ((uint16_t)(((uint16_t)d[0] << 8) + d[1]))
 #define get_u32(d) ((uint32_t)(((uint32_t)d[0] << 24) + \
 	((uint32_t)d[1] << 16) + \
 	((uint32_t)d[2] << 8) + \
