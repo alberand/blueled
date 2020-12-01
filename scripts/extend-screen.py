d3ef330586ff9b17508a75e59fb3b48b25e461e7
alberand <albershteyn.andrey@gmail.com>
2020-12-01 14:28:49 +0100

    add: script for simulation of ambient light

--- scripts/extend-screen.py
+++ scripts/extend-screen.py
@@ -20,54 +20,54 @@ num_leds = 120
 c.set_leds(num_leds)
 
 def get_borders(img, size, num_segments=5):
-	bw = 5
-	mask = np.zeros(img.shape[0:2], dtype=bool)
-	mask[mask.ndim * (slice(bw, -bw),)] = True 
+    bw = 5
+    mask = np.zeros(img.shape[0:2], dtype=bool)
+    mask[mask.ndim * (slice(bw, -bw),)] = True 
 
-	top = img[0:bw, 0:img.shape[1]]
-	right = np.transpose(img[bw:(img.shape[0] - bw), (img.shape[1] - bw):img.shape[1]], (1, 0, 2))
-	bottom = np.flip(img[img.shape[0] - bw:img.shape[0], 0:img.shape[1]], 1)
-	left = np.flip(np.transpose(img[bw:(img.shape[0] - bw), 0:bw], (1, 0, 2)), 1)
-	final = np.hstack([top, right, bottom, left])
-	
-	gradient = skimage.transform.resize(final, (1, int(size/num_segments)), 
+    top = img[0:bw, 0:img.shape[1]]
+    right = np.transpose(img[bw:(img.shape[0] - bw), (img.shape[1] - bw):img.shape[1]], (1, 0, 2))
+    bottom = np.flip(img[img.shape[0] - bw:img.shape[0], 0:img.shape[1]], 1)
+    left = np.flip(np.transpose(img[bw:(img.shape[0] - bw), 0:bw], (1, 0, 2)), 1)
+    final = np.hstack([top, right, bottom, left])
+    
+    gradient = skimage.transform.resize(final, (1, int(size/num_segments)), 
             anti_aliasing=False, order=3)
-	
-	return gradient
+    
+    return gradient
 
 def rgb2int(rgb):
-	return (int(rgb[0]*255) << 16) + (int(rgb[1]*255) << 8) + (int(rgb[2]*255) << 0)
+    return (int(rgb[0]*255) << 16) + (int(rgb[1]*255) << 8) + (int(rgb[2]*255) << 0)
 
 def int2rgb(num):
-	return (((num >> 16) & 0xFF)/255, ((num >> 8) & 0xFF)/255, ((num >> 0) & 0xFF)/255)
+    return (((num >> 16) & 0xFF)/255, ((num >> 8) & 0xFF)/255, ((num >> 0) & 0xFF)/255)
 
 def loop():
-	with mss() as sct:
-		for _ in range(10):
-			f = sct.shot()
+    with mss() as sct:
+        #for _ in range(10):
+        while True:
+            f = sct.shot()
 
-			# Read create screenshot
-			img = skimage.io.imread(f)
-			small = skimage.transform.resize(img, (108, 192), anti_aliasing=True)
+            # Read create screenshot
+            img = skimage.io.imread(f)
+            small = skimage.transform.resize(img, (108, 192), anti_aliasing=True)
 
-			# Blur
-			sigma = 3
-			blurred = skimage.filters.gaussian(small, sigma=sigma, truncate=3.5, multichannel=True)
+            # Blur
+            sigma = 3
+            blurred = skimage.filters.gaussian(small, sigma=sigma, truncate=3.5, multichannel=True)
 
-			segments = get_borders(small, num_leds, 15)
+            segments = get_borders(small, num_leds, 15)
 
-			colors = [rgb2int(c) for c in segments[0]]
-			c.segments(*colors)
-			# c.solid(colors[0])
+            colors = [rgb2int(c) for c in segments[0]]
+            c.segments(*colors)
 
-			time.sleep(0.5)
+            time.sleep(0.035)
 
 if __name__ == '__main__':
-	print('Extend screen with LEDs')
+    print('Extend screen with LEDs')
 
-	try:
-		loop()
-	except KeyboardInterrupt as e:
-		print('Exiting')
-		print(e)
-		c.stop()
+    try:
+        loop()
+    except KeyboardInterrupt as e:
+        print('Exiting')
+        print(e)
+        c.stop()
